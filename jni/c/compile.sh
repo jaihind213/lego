74f0f760f1d8befe505f6b51704c97ff4b7db78a
jaihind213 <vishnuhr@flipkart.com>
2014-02-16 18:33:03 +0530

    commit for sample jni source code

---
+++ jni/c/compile.sh
@@ -0,0 +1,3 @@
+### jni.h is located at "/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Headers"
+
+cc -framework JavaVM -I/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Headers  SampleJni.c
---
+++ jni/java_src/com/jni/Hello.java
@@ -0,0 +1,13 @@
+package com.jni;
+public class Hello{
+
+public static void hi(){
+    System.out.println("-----method hi from class Hello-----");
+}
+
+public static void main(String args[]){
+    hi();
+}
+
+
+}
