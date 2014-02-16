c4d9ab23f66786e63ea84ff042d06b7c3510bf52
jaihind213 <vishnuhr@flipkart.com>
2014-02-16 19:04:14 +0530

    learning about how 'java static objects' works via c to java jni calls

--- jni/c/SampleJni.c
+++ jni/c/SampleJni.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <jni.h>
 
-JNIEnv* create_vm() {
+JNIEnv* create_vm(char * class_path) {
 	JavaVM* jvm;
 	JNIEnv* env;
 	JavaVMInitArgs args;
@@ -10,7 +10,7 @@ JNIEnv* create_vm() {
 	/* There is a new JNI_VERSION_1_4, but it doesn't add anything for the purposes of our example. */
 	args.version = JNI_VERSION_1_6;
 	args.nOptions = 1;
-	options[0].optionString = "-Djava.class.path=/Users/vishnuhr/volume/mysql-r2d2/jni/classes";
+	options[0].optionString = class_path;
 	args.options = options;
 	args.ignoreUnrecognized = JNI_FALSE;
 
@@ -18,28 +18,40 @@ JNIEnv* create_vm() {
 	return env;
 }
 
-void invoke_class(JNIEnv* env) {
+void invoke_class(JNIEnv* env, char * class_name, char * method_name) {
  
 	jclass helloWorldClass;
 	jmethodID mainMethod;
 	jobjectArray applicationArgs;
 	jstring applicationArg0;
 
-	helloWorldClass = (*env)->FindClass(env, "com/jni/Hello");
+	helloWorldClass = (*env)->FindClass(env, class_name);
 
-	mainMethod = (*env)->GetStaticMethodID(env, helloWorldClass, "main", "([Ljava/lang/String;)V");
+	mainMethod = (*env)->GetStaticMethodID(env, helloWorldClass, method_name, "([Ljava/lang/String;)V");
 
+        //type of argument -> string[]
 	applicationArgs = (*env)->NewObjectArray(env, 1, (*env)->FindClass(env, "java/lang/String"), NULL);
-	applicationArg0 = (*env)->NewStringUTF(env, "From-C-program");
+	//0th member of string array ->
+        applicationArg0 = (*env)->NewStringUTF(env, "From-C-program");
+
 	(*env)->SetObjectArrayElement(env, applicationArgs, 0, applicationArg0);
+
+
         printf("calling java method from c.....\n");
+	(*env)->CallStaticVoidMethod(env, helloWorldClass, mainMethod, applicationArgs);
+	
+        printf("calling java method from c again.....\n");
 	(*env)->CallStaticVoidMethod(env, helloWorldClass, mainMethod, applicationArgs);
 }
 
 
 int main(int argc, char **argv) {
-	JNIEnv* env = create_vm();
-	invoke_class( env );
+printf("This program helps you call static methods present in java objects.. \n"); printf("\n");
+	JNIEnv* env = create_vm("-Djava.class.path=../classes");
+
+        char * className = "com/jni/Hello";
+        char * methodName = "hi";
+	invoke_class( env, className, methodName );
 }
 
 
--- jni/java_src/com/jni/Hello.java
+++ jni/java_src/com/jni/Hello.java
@@ -1,12 +1,20 @@
 package com.jni;
 public class Hello{
 
-public static void hi(){
-    System.out.println("-----method hi from class Hello-----");
+public static Integer count =0;
+
+private static Hello instance = new Hello();
+
+public static void hi(String args[]){
+    count++;
+    System.out.println("      -----method hi from class Hello-----This method was called for the "+count+ " 'th time.");
+    System.out.println("           -----method hi from class Hello-----got parameter:"+args[0]);
+    System.out.println("The hashcode of THE private Static Hello instance is : "+ instance.hashCode());
 }
 
 public static void main(String args[]){
-    hi();
+    hi(args);
+
 }
 
 
